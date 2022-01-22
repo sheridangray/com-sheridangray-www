2ee68ce288e323e2ced34b560f3ca444d7421fca
SHERIDAN GRAY <sheridangray@Silver-Fox.local>
2022-01-22 09:54:31 -0800

    Updated hompage and added images

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -20,7 +20,7 @@ const HomePage = () => {
                                 <h4>Husband & Father | Business & Technology</h4>
                             </header>
                             <p>Welcome to my little part of the web.</p>
-                            <p>Professionally, I am a Product Manager at <a href="https://www.meta.com" target="_blank" rel="noreferrer" >Meta</a>. I focus on building privacy products at scale across our apps and products. I also partially own <a href="https://www.capiogroup.com" target="_blank" rel="noreferrer" >Capio Group</a>, an IT consulting firm in Sacramento, CA that provides technology and project management services to California State agencies.</p>
+                            <p>Professionally, I am a Product Manager at <a href="https://www.meta.com" target="_blank" rel="noreferrer" >Meta</a>. I focus on building privacy products at scale across our apps and products. I also founded <a href="https://www.capiogroup.com" target="_blank" rel="noreferrer" >Capio Group</a>, an IT consulting firm in Sacramento, CA that provides technology and project management services to California State agencies.</p>
                             <p>I obtained an MBA from <a href="https://mitsloan.mit.edu/" target="_blank" rel="noreferrer" >MIT Sloan</a> in Cambridge, MA and a a Bachelor's Degree in Business Administration with an emphasis on Accounting from the <a href="https://www.uw.ed" target="_blank" rel="noreferrer" >University of Washington</a> in Seattle, WA.</p>
                             
                             <p>I currently live in San Francisco, CA with my wife and son.</p>
