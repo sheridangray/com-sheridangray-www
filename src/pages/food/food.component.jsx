9775d6220f1d879c4b1241d88e3878d86f125e3d
Nikitakan <nikitakanwardev@gmail.com>
2022-06-27 17:32:35 +0530

    calendar component creation and some style changes

--- src/pages/food/food.component.jsx
+++ src/pages/food/food.component.jsx
@@ -7,87 +7,47 @@ import fried from "../../assets/meals/fried.png";
 
 import "./food.styles.scss";
 import ChooseMealsPage from "../../components/choose-meals/choosemeals.component";
-
+import {
+  add,
+  eachDayOfInterval,
+  endOfMonth,
+  format,
+  getDay,
+  isEqual,
+  isSameDay,
+  isSameMonth,
+  isToday,
+  parse,
+  parseISO,
+  startOfToday,
+} from 'date-fns'
 const FoodPage = () => {
-  
-  const currentYear = new Date().getFullYear();
-  const currnetMonth = new Date().getMonth()   
-  function getDaysArray(year, month) {
-    var numDaysInMonth, daysInWeek, daysIndex, index, i, l, daysArray;
-
-    numDaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    daysInWeek = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    daysIndex = { Sun: 0, Mon: 1, Tue: 2, Wed: 3, Thu: 4, Fri: 5, Sat: 6 };
-    index = daysIndex[new Date(year, month - 1, 1).toString().split(" ")[0]];
-    daysArray = [];
-
-    for (i = 0, l = numDaysInMonth[month - 1]; i < l; i++) {
-      daysArray.push({ date: i + 1, day: daysInWeek[index++] });
-      if (index == 7) index = 0;
-    }
-
-    return daysArray;
-  }
-  function getWeekDates(currentNum) {
-
-    let now = new Date(currentNum);
-    let dayOfWeek = now.getDay(); //0-6
-    let numDay = now.getDate();
-  
-    let start = new Date(now); //copy
-    start.setDate(numDay - dayOfWeek);
-    start.setHours(0, 0, 0, 0);
-  
-  
-    let end = new Date(now); //copy
-    end.setDate(numDay + (7 - dayOfWeek));
-    end.setHours(0, 0, 0, 0);
-  
-    return [start.getDate(), end.getDate()];
-  }
-
   const [currentNum,setCurrentNum] = useState(new Date().getDate());
-  const [startDate,setStartDate] = useState(new Date().getDate());
-  const [endDate,setEndDate] = useState(new Date().getDate());
-  const [modalView,setModalView] = useState(false);
+  const [modalView, setModalView] = useState(false);
+  let today = startOfToday()
+  let [selectedDay, setSelectedDay] = useState(today)
+  let [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'))
+  let firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
 
-  useEffect(() => {
-    let [start, end] = getWeekDates(currentNum);
-    setStartDate(start);
-    setEndDate(end)
-  },[currentNum])
-  
-  
+  let days = eachDayOfInterval({
+    start: firstDayCurrentMonth,
+    end: endOfMonth(firstDayCurrentMonth),
+  })
 
-
-  const paginate = () => {
-
-    setCurrentNum((p) => p+7);
-    // getWeekDates(currentNum);
-
+  function previousMonth() {
+    
+    let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 })
+    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
   }
 
-  const paginateNeg = () => {
-
-    setCurrentNum((p) => p-7);
-
+  function nextMonth() {
+    let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
+    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
   }
-  
-//   console.log(start.toLocaleString(), end.toLocaleString());
 
-
-  const monthData = getDaysArray(currentYear, currnetMonth+1);
-  console.log(monthData)
-
-  
+  // let selectedDayMeetings = meetings.filter((meeting) =>
+  //   isSameDay(parseISO(meeting.startDatetime), selectedDay)
+  // )
   return (
     <div className="meal_plan_row">
       <div className="recipee_sidebar">
@@ -107,18 +67,20 @@ const FoodPage = () => {
             <h4>Meal Planner</h4>
             <button>Today</button>
             <div className="arrows">
-              <span>
-                <img src={arrow} onClick={() => paginateNeg()}/>{" "}
-              </span>
-              <span>
-                <img src={rightarrow}  onClick={() => paginate()}/>{" "}
-              </span>
+              <button onClick={previousMonth}>
+                <img src={arrow} />
+              </button>
+              <button onClick={nextMonth}>
+                <img src={rightarrow} />
+              </button>
             </div>
-            <span className="time_between">June {startDate} - {endDate}</span>
+            <span className="time_between">
+            {format(firstDayCurrentMonth, 'MMMM yyyy')}
+            </span>
           </div>
           <div className="lunch_type">
             <label>
-              <input type="radio" name="food"></input>
+              <input type="radio" name="food" checked></input>
               <span></span>
               Breakfast
             </label>
@@ -135,25 +97,27 @@ const FoodPage = () => {
           </div>
         </div>
         <div className="mealsday_row">
-          {monthData.slice(currentNum,currentNum+7).map((e) => <div className="meals_col">
-            <h6 className="meal_day">{e.date}</h6>
-            <h5 className="meals_date">{e.day}</h5>
-            <div className="dish_with_swap">
-              <img src={fried} />
+         {days.map((day)=>  <div className="meals_col">
+              <h6 className="meal_day">{format(new Date(day), 'EEEE')}</h6>
+              <h5 className="meals_date"><time dateTime={format(day, 'yyyy-MM-dd')}>
+                      {format(day, 'd')}
+                    </time></h5>
+              <div className="dish_with_swap">
+                <img src={fried} />
 
-              <h4 className="dishname">Fried Eggs</h4>
-              <div className="swap_rate">
-                <span>Swap</span>
-                <span onClick={() => setModalView(true)}>Choose</span>
-                <span>Rate</span>
+                <h4 className="dishname">Fried Eggs</h4>
+                <div className="swap_rate">
+                  <span>Swap</span>
+                  <span onClick={() => setModalView(true)}>Choose</span>
+                  <span>Rate</span>
+                </div>
               </div>
-            </div>
-          </div>)}
-
-          {
-              modalView && <ChooseMealsPage setModalView={setModalView}/>
-          }  
+            </div>)}
+           
           
+
+          {modalView && <ChooseMealsPage setModalView={setModalView} />}
+
           {/* <div className="meals_col">
             <h6 className="meal_day">SAT</h6>
             <h5 className="meals_date">18</h5>
@@ -239,7 +203,7 @@ const FoodPage = () => {
               </div>
             </div>
           </div>*/}
-        </div> 
+        </div>
       </div>
     </div>
   );
